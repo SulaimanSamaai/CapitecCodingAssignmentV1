45f2e866928499b4eb79925d4e5931849f9678de
SulaimanSamaai <42357234+SulaimanSamaai@users.noreply.github.com>
2018-08-13 22:49:19 +0200

    Initial Checkin

---
+++ CapitecCodingAssignment-master/CapitecCodingAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Helpers;
+
+namespace CapitecCodingAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SetupConsole();
+
+            try
+            {
+                ValidateInput.ValidateCommandLineArguments(args);
+
+                new ProcessTweets.Process()
+                {
+                    UserFile = Path.Combine(Settings.SourceFileLocation, args[0]),
+                    TweetFile = Path.Combine(Settings.SourceFileLocation, args[1])
+                }.ProcessFiles();
+            }
+            catch (ArgumentException ex)
+            {
+                ConsoleHelper.WriteToConsole(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                ConsoleHelper.WriteToConsole(ex.Message);
+            }
+            finally
+            {
+                ConsoleHelper.WriteToConsole("Press enter to continue...");
+                var consoleInput = ConsoleHelper.ReadFromConsole();
+            }
+        }
+
+        private static void SetupConsole()
+        {
+            Console.Title = "Capitec Coding Assignment";
+            Console.WindowWidth = Settings.ConsoleWidth;
+            Console.WindowHeight = Settings.ConsoleHeight;
+        }
+    }
+}
